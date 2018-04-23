6402ea8999d3a2b6655f6f11e2a9b5f285ea2282
Travis Kane <kevinthomaskane@gmail.com>
2018-04-23 14:59:11 -0700

    event page has basic layout and methods created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Event from "./components/Event"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Event />
       </div>
     );
   }
