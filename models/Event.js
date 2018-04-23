29054eda32d923db027a240041bb3f791cee6e26
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-23 12:27:47 -0700

    created the mysql tables in the backend

---
+++ models/Event.js
@@ -0,0 +1,48 @@
+module.exports = function(sequelize, DataTypes) {
+  const Even = sequelize.define("Event", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.BLOB('long'),
+      allowNull: true
+    },
+    sportType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: DataTypes.TEXT,
+    address: DataTypes.INTEGER,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zip: DataTypes.INTEGER
+  });
+
+  Even.associate = function(models) {
+    const User = models.User;
+    Even.belongsToMany(User, {
+      through: "Host",
+      foreignKey: "eventId"
+    });
+
+    Even.belongsToMany(User, {
+      through: "Attendee",
+      foreignKey: "eventId"
+    });
+
+    Even.hasMany(models.Chat, {
+      foreignKey: {
+        name: "event_id",
+        allowNull: false
+      }
+    });
+
+  };
+
+  return Even;
+};
--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+const db = require("./models");
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(cookieParser());
+app.use(fileUpload());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -14,6 +26,8 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+db.sequelize.sync().then(function() {
+  var server = app.listen(PORT, function() {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
+  });
 });
