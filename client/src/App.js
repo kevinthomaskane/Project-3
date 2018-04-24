6ddaa6237a5140b963bda1e7d964d7fe82bed539
Guswilliams <gus.williams1@outlook.com>
2018-04-24 10:36:06 -0700

    mainpage push

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-
+import Mainpage from "./components/mainpage"
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Mainpage} />
+          <Route exact path="/profile/:id" component={Profile} />
+          <Route exact path="/events/:id" component={Events} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
+    </Router>
+    
     );
   }
 }
