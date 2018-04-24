696b44bb53551c9ea02cf706f227ba2d2c2014db
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-24 11:15:13 -0700

    cleaning up mainpage

--- client/src/components/mainpage/mainpage.js
+++ client/src/components/mainpage/mainpage.js
@@ -1,44 +1,41 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Axios from "axios"
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class Mainpage extends Component {
-    state = {
-        events: [],
+  state = {
+    events: []
+  };
 
-    };
+  componentDidMount() {
+    Axios.get("/api/events").then(res => this.setState({
+      events: [
+        {
+          id: 1,
+          content: "event 1",
+          image: "random.com"
+        }
+      ]
+    })).catch(err => console.log(err));
 
-    componentDidMount() {
-        Axios.get("/api/events")
-            .then(res =>
-                this.setState({ events: [{id: 1, content: "event 1", image: "random.com"}] })
-            )
-            .catch(err => console.log(err));
+  }
 
-    }
+  loadEvents = () => {};
 
-    loadEvents = () => {
-
-    };
-
-    render() {
-        return (
-
-            <ul className="collection">
-                {this.state.events.map(function (event) {
-                    return (
-                        <Link to={"/event/" + event.id}>
-                            <li className="collection-item avatar">
-                                <img className="circle" src={event.image} />
-                                <p>{event.content}</p>
-                            </li>
-                        </Link>
-                    )
-                })}
-            </ul>
-        )
-    }
-
+  render() {
+    return (<ul className="collection">
+      {
+        this.state.events.map(function(event, index) {
+          return (<Link to={"/event/" + event.id}>
+            <li key={index} className="collection-item avatar">
+              <img className="circle" src={event.image}/>
+              <p>{event.content}</p>
+            </li>
+          </Link>)
+        })
+      }
+    </ul>)
+  }
 
 }
 
