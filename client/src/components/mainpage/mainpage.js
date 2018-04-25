217c4e604162c386df994eaaa4b7cecfe9cd7e6f
Guswilliams <gus.williams1@outlook.com>
2018-04-25 10:05:02 -0700

    filename updates

--- client/src/components/mainpage/mainpage.js
+++ client/src/components/mainpage/mainpage.js
@@ -1,42 +1,104 @@
 import React, {Component} from "react";
 import Axios from "axios"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Navbar, Icon, Col, Row, NavItem, CardPanel } from "react-materialize"
 
 class Mainpage extends Component {
-  state = {
-    events: []
-  };
+    state = {
+        events: [],
+        searching: false,
+        searchterm: ""
 
-  componentDidMount() {
-    Axios.get("/api/events").then(res => this.setState({
-      events: [
-        {
-          id: 1,
-          content: "event 1",
-          image: "random.com"
-        }
-      ]
-    })).catch(err => console.log(err));
+    };
 
-  }
+    componentDidMount() {
+        Axios.get("/api/events")
+            .then(res =>
+                this.setState({ events: [{ id: 1, content: "event 1", image: "random.com" }] })
+            )
+            .catch(err => console.log(err));
 
-  loadEvents = () => {};
+    }
 
-  render() {
-    return (<ul className="collection">
-      {
-        this.state.events.map(function(event, index) {
-          return (<Link to={"/event/" + event.id}>
-            <li key={index} className="collection-item avatar">
-              <img className="circle" src={event.image}/>
-              <p>{event.content}</p>
-            </li>
-          </Link>)
+    loadEvents = () => {
+
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+    handleSubmitChange = event => {
+        event.preventDefault()
+        //    Axios.get("")
+    }
+
+    togglesearch = () => {
+        this.state.searching ? this.setState({
+            searching: false
+        }) : this.setState({
+            searching: true
         })
-      }
-    </ul>)
-  }
+    }
 
+
+    render() {
+        return (
+            <div>
+                <Navbar brand='logo' right>
+
+                    {this.state.searching ? <form onSubmit={this.handleSubmitChange} > <input onChange={this.handleInputChange} id="searchbox" name="searchterm" type="text"></input></form> : ""}
+                    <NavItem onClick={this.togglesearch}><Icon>search</Icon></NavItem>
+                </Navbar>
+
+                <Row>
+                    <Col s={12} m={6}>
+                        <CardPanel className="teal lighten-4 black-text">
+                            <span>Event Image</span>
+                        </CardPanel>
+                    </Col>
+
+                    <Col s={12} m={6}>
+                        <CardPanel className="teal lighten-4 black-text">
+                            <span>Event Image</span>
+                        </CardPanel>
+                    </Col>
+
+                </Row>
+
+                <Row>
+                    <Col s={12} m={6}>
+                        <CardPanel className="teal lighten-4 black-text">
+                            <span>Event Image</span>
+                        </CardPanel>
+                    </Col>
+
+
+                    <Col s={12} m={6}>
+                        <CardPanel className="teal lighten-4 black-text">
+                            <span>Event Image</span>
+                        </CardPanel>
+                    </Col>
+
+                </Row>
+
+                <ul className="collection">
+                    {this.state.events.map(function (event) {
+                        return (
+                            <Link to={"/event/" + event.id}>
+                                <li className="collection-item avatar">
+                                    <img className="circle" src={event.image} />
+                                    <p>{event.content}</p>
+                                </li>
+                            </Link>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Mainpage;
