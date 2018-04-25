493154d0a761bd876b77cb34bc711ffcf05bffea
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-04-25 11:31:32 -0700

    nothing

--- client/src/components/MapContainer/MapContainer.js
+++ client/src/components/MapContainer/MapContainer.js
@@ -1,31 +1,22 @@
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React, { Component } from 'react';
 import axios from "axios";
-// import mapTest from "../../mapTest.json";
 
-// const addy = `${this.state.streetAddress} ${this.state.city} ${this.state.city} ${this.state.zip}`;
+
 export class MapContainer extends Component {
 
     // state = {
     //     address: ""
     // };
 
+    onMouseoverMarker = (props, marker, e) => {
+
+    }
+
+
       getInfo = () => {
         axios.get("/api/events").then((response) => {
           console.log(response.data);
-          // for (var i = 0; i < response.data.length; i++) {
-          //   let addy = `${response.data[i].address} ${response.data[i].city} ${response.data[i].state}`;
-          // }
-          // console.log(addy);
-          // axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addy}CA&key=AIzaSyDKYcYNqOJapazYjjFKVq3t94ljuBhx67o`
-          // ).then((response) => {
-          //   console.log(response);
-          //   this.setState({ address : {
-          //     lat: response.data.results[0].geometry.location.lat,
-          //     lng: response.data.results[0].geometry.location.lng
-          //   }
-          //   })
-          // })
         });
       };
 
@@ -39,11 +30,12 @@ export class MapContainer extends Component {
 
         {this.state}
 
-       <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+        <Marker 
+          title={'Current Location'}
+          position={'Current Location'}
+        />
 
-       <Marker onClick={this.onMarkerClick}
-                name="test" />
+       
 
       </Map>
     );
