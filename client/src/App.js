891ce74dc444c8ffc72c7db40c9dbe0be7f57c66
Your Name <you@example.com>
2018-04-25 11:10:56 -0700

    fixed components

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Even from "./components/Event";
-import mainpage from "./components/mainpage";
+import Mainpage from "./components/Mainpage/";
 import Header from "./components/Header";
 import MapContainer from "./components/MapContainer"
 
@@ -12,9 +12,10 @@ class App extends Component {
     return (
       <Router>
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/login" component={Login}/>
-        <Route exact path="/" component={mainpage} />
+        <Route exact path="/" component={Mainpage} />
         {/* <Route exact path="/profile/:id" component={Profile} /> */}
         <Route exact path="/event/:id" component={Even} />
         <Route exact path="/map" component={MapContainer} />
--- client/src/components/signUp/SignUp.js
+++ client/src/components/signUp/SignUp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-
+import "./SigneUp.css";
 class SignUp extends React.Component{
 
     state = {
@@ -55,36 +55,34 @@ class SignUp extends React.Component{
 
     render(){
         return(
-            <div id="loginForm" class="row">
+          <div id="loginForm" class="row">
             <form class="col s12">
               <div class="row">
                 <div class="input-field col s6">
-                  <input onChange={this.handleFirstNameInputChange} placeholder="First Name" id="first_name"  value={this.state.Firstname}type="text" class="validate"/>
+                  <input onChange={this.handleFirstNameInputChange} placeholder="First Name" id="first_name" value={this.state.Firstname} type="text" class="validate" />
                 </div>
                 <div class="input-field col s6">
-                  <input onChange={this.handleLastNameInputChange} placeholder="Last Name "id="last_name" value={this.state.LastName} type="text" class="validate"/>
+                  <input onChange={this.handleLastNameInputChange} placeholder="Last Name " id="last_name" value={this.state.LastName} type="text" class="validate" />
                 </div>
               </div>
               <div class="row">
                 <div class="input-field col s12">
-                  <input onChange={this.handlePasswordInputChange} placeholder="Password" id="password" value={this.state.Password} type="password" class="validate"/>
+                  <input onChange={this.handlePasswordInputChange} placeholder="Password" id="password" value={this.state.Password} type="password" class="validate" />
                 </div>
               </div>
               <div class="row">
                 <div class="input-field col s12">
-                  <input onChange={this.handleEmailInputChange} placeholder="Email" id="email" value={this.state.Email} type="email" class="validate"/>
+                  <input onChange={this.handleEmailInputChange} placeholder="Email" id="email" value={this.state.Email} type="email" class="validate" />
                 </div>
               </div>
-               <form  id="uploadImg" action="/api/upload" method="post" enctype="multipart/form-data">
-               <input type="file" name="uploadFile"/>
-               <input  class="btn btn-success" type="submit" value="Upload!"/>
-               <input id="hiddenInput" type="hidden" value="32" name="userId"/>
-               </form>
+              <form id="uploadImg" action="/api/upload" method="post" enctype="multipart/form-data">
+                <input type="file" name="uploadFile" />
+                <input class="btn btn-success" type="submit" value="Upload!" />
+                <input id="hiddenInput" type="hidden" value="32" name="userId" />
+              </form>
             </form>
-            </div>
-            
+          </div>  
         )
-
     };
 };
 
