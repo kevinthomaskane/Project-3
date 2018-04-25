6c51660b85debbb4bad08f6af480ba70cb3f3745
Guswilliams <gus.williams1@outlook.com>
2018-04-25 13:31:17 -0700

    recent changes to main

--- client/src/components/mainpage/mainpage.js
+++ client/src/components/mainpage/mainpage.js
@@ -1,13 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Axios from "axios"
 import { Link } from "react-router-dom";
-import { Navbar, Icon, Col, Row, NavItem, CardPanel } from "react-materialize"
+import { Navbar, Icon, Col, Row, NavItem, CardPanel, Button } from "react-materialize"
 
 class Mainpage extends Component {
     state = {
-        events: [],
+        events: [{ id: 1, content: "event 1", image: "random.com" }, { id: 1, content: "event 2", image: "random.com" }, { id: 1, content: "event 3", image: "random.com" }],
         searching: false,
-        searchterm: ""
+        searchterm: "",
+        filtered: [],
+        clicked: false
 
     };
 
@@ -23,6 +25,12 @@ class Mainpage extends Component {
     loadEvents = () => {
 
     };
+
+    getFiltered = (sportType) => {
+        Axios.get("/api/events/" + sportType).then((response) => {
+            this.setState({ events: response.data, clicked: true })
+        })
+    }
 
     handleInputChange = event => {
         const { name, value } = event.target;
@@ -43,59 +51,54 @@ class Mainpage extends Component {
         })
     }
 
+    toggleFiltered = () => {
+        this.state.filtered ? this.setState({
+            filtered: false
+        }) : this.setState({
+            filtered: true
+        })
+    }
 
     render() {
         return (
             <div>
                 <Navbar brand='logo' right>
-
                     {this.state.searching ? <form onSubmit={this.handleSubmitChange} > <input onChange={this.handleInputChange} id="searchbox" name="searchterm" type="text"></input></form> : ""}
                     <NavItem onClick={this.togglesearch}><Icon>search</Icon></NavItem>
                 </Navbar>
 
-                <Row>
-                    <Col s={12} m={6}>
-                        <CardPanel className="teal lighten-4 black-text">
-                            <span>Event Image</span>
-                        </CardPanel>
-                    </Col>
-
-                    <Col s={12} m={6}>
-                        <CardPanel className="teal lighten-4 black-text">
-                            <span>Event Image</span>
-                        </CardPanel>
-                    </Col>
-
-                </Row>
+                <Button onClick={this.toggleFiltered}>Flag Football</Button>
+                <Button onClick={this.toggleFiltered}>Frisbee</Button>
+                <Button onClick={this.toggleFiltered}>Basketball</Button>
+                <Button onClick={this.toggleFiltered}>Soccer</Button>
 
                 <Row>
-                    <Col s={12} m={6}>
-                        <CardPanel className="teal lighten-4 black-text">
-                            <span>Event Image</span>
-                        </CardPanel>
-                    </Col>
-
-
-                    <Col s={12} m={6}>
-                        <CardPanel className="teal lighten-4 black-text">
-                            <span>Event Image</span>
-                        </CardPanel>
-                    </Col>
-
-                </Row>
-
-                <ul className="collection">
-                    {this.state.events.map(function (event) {
+                    {this.state.clicked === false ? this.state.events.map(function (event) {
                         return (
                             <Link to={"/event/" + event.id}>
-                                <li className="collection-item avatar">
-                                    <img className="circle" src={event.image} />
-                                    <p>{event.content}</p>
-                                </li>
+                                <Col s={12} m={6}>
+                                    <CardPanel className="teal lighten-4 black-text">
+                                        <img className="circle" src={event.image} />
+                                        <p>{event.content}</p>
+                                    </CardPanel>
+                                </Col>
                             </Link>
                         )
-                    })}
-                </ul>
+                    }) : ""}
+                </Row>
+
+                {/* <Row>
+                    {this.state.toggleFiltered === false ? this.state.events.map(function (event) {
+                        return (
+                            <Link to={"/event/" + event.id}>
+                                <Button onClick={this.toggleFiltered}>Flag Football</Button>
+                                <Button onClick={this.toggleFiltered}>Frisbee</Button>
+                                <Button onClick={this.toggleFiltered}>Basketball</Button>
+                                <Button onClick={this.toggleFiltered}>Soccer</Button>
+                            </Link>
+                        )
+                    }) : ""}
+                </Row> */}
             </div>
         )
     }
