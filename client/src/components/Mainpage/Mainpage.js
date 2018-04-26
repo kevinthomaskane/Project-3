8323eaf9074494ca38fc17e1bdd8f5e4e283ad09
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-04-26 11:51:01 -0700

    css changed on mainpage

--- client/src/components/Mainpage/Mainpage.js
+++ client/src/components/Mainpage/Mainpage.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Axios from "axios"
 import { Link } from "react-router-dom";
 import { Navbar, Icon, Col, Row, NavItem, CardPanel, Button } from "react-materialize"
+import "./Mainpage.css";
+
 
 class Mainpage extends Component {
     state = {
@@ -68,25 +70,20 @@ class Mainpage extends Component {
                     <NavItem onClick={this.togglesearch}><Icon>search</Icon></NavItem>
                 </Navbar> */}
 
-
-                <Button onClick={()=>this.toggleFiltered("Flag Football")}>Flag Football</Button>
-                <Button onClick={()=>this.toggleFiltered("Frisbee")}>Frisbee</Button>
-                <Button onClick={()=>this.toggleFiltered("Basketball")}>Basketball</Button>
-                <Button onClick={()=>this.toggleFiltered("Soccer")}>Soccer</Button>
-        <Row>
-             <Col s={3} >
-                <Button className="EventButton" onClick={this.toggleFiltered}>Flag Football</Button>
-             </Col>
-             <Col s={3} >
-                <Button className="EventButton" onClick={this.toggleFiltered}>Frisbee</Button>
-             </Col>
-             <Col s={3} >
-                <Button className="EventButton" onClick={this.toggleFiltered}>Basketball</Button>
-             </Col>
-             <Col s={3} >
-                <Button className="EventButton" onClick={this.toggleFiltered}>Soccer</Button>
-             </Col>
-        </Row>
+            <Row>
+                <Col s={3} >
+                <Button className="EventButton" onClick={()=>this.toggleFiltered("Flag Football")}><img className="icon" src={"../images/football.png"}/>Flag Football</Button>
+                </Col>
+                <Col s={3} >
+                <Button className="EventButton" onClick={()=>this.toggleFiltered("Frisbee")}><img className="icon" src={"../images/fris.png"}/>Frisbee</Button>
+                </Col>
+                <Col s={3} >
+                <Button className="EventButton" onClick={()=>this.toggleFiltered("Basketball")}><img className="icon" src={"../images/bball.png"}/>Basketball</Button>
+                </Col>
+                <Col s={3} >
+                <Button className="EventButton" onClick={()=>this.toggleFiltered("Soccer")}><img className="icon" src={"../images/soccer.png"}/>Soccer</Button>
+                </Col>
+                </Row>
 
                 <Row>
                     {this.state.clicked === false ? this.state.events.map(function (event, index) {
@@ -94,7 +91,7 @@ class Mainpage extends Component {
                         return (
                             <Link to={"/event/" + event.id}>
                                 <Col s={12} m={6}>
-                                    <CardPanel className="teal lighten-4 black-text">
+                                    <CardPanel className="EventCard">
                                         <img className="circle" src={event.image} />
                                         <p>{event.content}</p>
                                     </CardPanel>
